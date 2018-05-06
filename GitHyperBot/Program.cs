b3b19b3266a76e3c1312d007351c7e8ae9f4d9c3
hyper <leo_xarroweb@hotmail.com>
2018-05-06 15:22:21 -0300

    Iniciando Projeto
    Bom... Aqui temos o começo de tudo

---
+++ GitHyperBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHyperBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
