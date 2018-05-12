8ee0f6f1e6fdf2e8b38ea8119669c0db139fda24
hyper <leo_xarroweb@hotmail.com>
2018-05-12 14:02:17 -0300

    Criada conexão com o bot
    Agora o bot já se conecta ao discord por meio do token.
    Também gera um log.
    E está preparado para receber comandos, que serão adicionados posteriormente...

--- GitHyperBot/Program.cs
+++ GitHyperBot/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Reflection;
 using System.Threading.Tasks;
+using Discord;
 using Discord.WebSocket;
 using GitHyperBot.Core.Config;
 using GitHyperBot.Core.Handlers;
@@ -13,17 +13,22 @@ namespace GitHyperBot
         private CommandHandler _handler;
         private static void Main()
         {
+            Console.Title = "HyperBot v1.0";
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Inicializando HyperBot...");
+
+            //  Verificamos se o token existe
             if (Config.Bot.Token == null)
             {
+                //  Caso não exista
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("Por favor preencha o token no arquivo de configuração.");
                 Console.WriteLine("Esse arquivo se chama 'Data.json', e está presente na pasta 'Resources'\n");
                 Console.ResetColor();
                 return;     
             }
-            Console.WriteLine($"Token: {Config.Bot.Token}");
+
+            Console.WriteLine($"Token: {Config.Bot.Token}\n");
             Console.ResetColor();
 
             //  Iniciamos a classe "StartAsync" de forma assíncrona
@@ -32,7 +37,27 @@ namespace GitHyperBot
 
         public async Task StartAsync()
         {
+            //  Criamos um cliente
+            //  E atribuimos as suas devidas configurações
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                //  Loglevel é o nível minimo para ser gerado um log
+                LogLevel = LogSeverity.Verbose,
 
+                //  Tamanho máximo das mensagens a serem processadas pelo bot
+                //  Mais de 500 caracteres é ignorado
+                MessageCacheSize = 500
+            });
+
+            _client.Log += Core.Services.Logger.Log;
+            await _client.LoginAsync(TokenType.Bot, Config.Bot.Token);
+            await _client.StartAsync();
+
+            _handler = new CommandHandler();
+            await _handler.InitializeAsync(_client);
+
+            //  Previne do programa fechar
+            await Task.Delay(-1);
         }
     }
 }
