732a3f4839ab1f82c034e47211cccc1b305c3076
hyper <leo_xarroweb@hotmail.com>
2018-05-12 01:03:54 -0300

    Atualização do program.cs
    Atualização do program.cs para iniciar o desenvolvimento do bot

--- GitHyperBot/Program.cs
+++ GitHyperBot/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using GitHyperBot.Core.Config;
+using GitHyperBot.Core.Handlers;
 
 namespace GitHyperBot
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+        private CommandHandler _handler;
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Inicializando HyperBot...");
+            if (Config.Bot.Token == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Por favor preencha o token no arquivo de configuração.");
+                Console.WriteLine("Esse arquivo se chama 'Data.json', e está presente na pasta 'Resources'\n");
+                Console.ResetColor();
+                return;     
+            }
+            Console.WriteLine($"Token: {Config.Bot.Token}");
+            Console.ResetColor();
+
+            //  Iniciamos a classe "StartAsync" de forma assíncrona
+            new Program().StartAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task StartAsync()
+        {
+
         }
     }
 }
