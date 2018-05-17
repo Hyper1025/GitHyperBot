91a68cec36ddcd7c523c83f5b7bd58a03c03d5ef
hyper <leo_xarroweb@hotmail.com>
2018-05-17 13:34:16 -0300

    Adicionado AccountsDataStorage
    Responsável por manipular o arquivo das contas, em questão de salvar, serealizar e desserializar o objeto json das contas

---
+++ GitHyperBot/Core/DataManipulation/AccountsDataStorage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using GitHyperBot.Core.Databaset.User;
+using Newtonsoft.Json;
+
+namespace GitHyperBot.Core.DataManipulation
+{
+    public static class AccountsDataStorage
+    {
+        //  Salvar todas contas
+        //  Criamos uma classe publica e estática
+        //  que não retorna nada
+        //  Ela precisa de um argumento, que é o IEnumerable
+        //  Que basicamente é uma interface de coisas que você pode contar
+        //  E nos queremos uma interface contável de contas (accounts, que é a classe)
+        public static void SaveUsersAccounts(IEnumerable<AccountTemplate> accounts, string filePath)
+        {
+            //  Geramos o arquivo
+            string json = JsonConvert.SerializeObject(accounts, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+
+        public static IEnumerable<AccountTemplate> LoadUsersAccounts(string filePath)
+        {
+            //  Se o arquivo não existir, retornamos nulo
+            if (!File.Exists(filePath)) return null;
+
+            //  Caso contrário, seguimos
+            //  Lemos o arquivo para a string json
+            string json = File.ReadAllText(filePath);
+
+            //  Retornamos ele como uma lista "IEnumerable<AccountTemplate>"
+            return JsonConvert.DeserializeObject<List<AccountTemplate>>(json);
+        }
+
+        //  Verifica se o save já existe
+        //  Só temos essa função para não puxar o System.io para outra classe
+        public static bool SaveAlreadyExists(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+    }
+}
