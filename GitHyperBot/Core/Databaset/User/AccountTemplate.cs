ce33f5498c9e3dfc20403c24ea58fa70b6f4b554
hyper <leo_xarroweb@hotmail.com>
2018-05-17 13:32:44 -0300

    Adicionado AccountTemplate
    É uma "entidade" que servirá como "molde" para o arquivo das contas

---
+++ GitHyperBot/Core/Databaset/User/AccountTemplate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GitHyperBot.Core.Databaset.User
+{
+    public class AccountTemplate
+    {
+        //  Id do usuário
+        public ulong UserId { get; set; }
+
+        //  Moeda
+        public ulong Gold { get; set; }
+
+        //  Ultimo Daily
+        //  Já adicionamos um valor padrão, e removemos dois dias (24h)
+        //  
+        public DateTime LastDaily { get; set; } = DateTime.UtcNow.AddDays(-2);
+
+        //  Xp para sistema de experiência
+        public uint Xp { get; set; }
+
+        //  Número de advertências
+        public uint NumberOfWarning { get; set; }
+    }
+}
