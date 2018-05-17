9169496e018f30b3e1730157fcc230c1a90c1bb0
hyper <leo_xarroweb@hotmail.com>
2018-05-17 13:41:31 -0300

    Adicionado sistema de transferência no sistema de economia
    Método que permite um usuário dar dinheiro a outro usuário

---
+++ GitHyperBot/Features/Economia/Transfer.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.WebSocket;
+using GitHyperBot.Core.Databaset.User;
+
+namespace GitHyperBot.Features.Economia
+{
+    public class Transfer
+    {
+        internal enum Result
+        {
+            Success, SelfTransfer, NotEnoughMiunies, bot
+        }
+
+        internal static Result DeUserParaUseResult(IUser de, IUser para, ulong montanteUlong)
+        {
+            //  Verifica se foi a transferência foi realizada para a mesma pessoa
+            if (de.Id == para.Id) return Result.SelfTransfer;
+            if (para.IsBot) return Result.bot;
+
+            var fonte = AccountsMananger.GetAccount((SocketUser)de);
+
+            if (fonte.Gold < montanteUlong) return Result.NotEnoughMiunies;
+
+            var destino = AccountsMananger.GetAccount((SocketUser) para);
+
+            fonte.Gold -= montanteUlong;
+            destino.Gold += montanteUlong;
+
+            AccountsMananger.SaveAccounts();
+
+            return Result.Success;
+
+        }
+    }
+}
