adde073b1ff53f4f3aca914e9ebd011e18a49af4
hyper <leo_xarroweb@hotmail.com>
2018-05-17 13:51:40 -0300

    Adicionado módulo de comandos relacionados a configuração da guild
    Esse módulo ainda falta muito para ser concluído

---
+++ GitHyperBot/Modules/GuildConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using GitHyperBot.Core.Databaset.Server;
+using GitHyperBot.Core.Handlers;
+
+namespace GitHyperBot.Modules
+{
+    public class GuildConfig : ModuleBase<SocketCommandContext>
+    {
+        [Group("Definir"), Summary("Configuração da guild")]    //  Criamos um grupo para definir configurações
+        [RequireUserPermission(GuildPermission.Administrator)]  //  Requerimos permissão de adm para usar esse grupo de cmd's
+        public class ConfigGruoup : ModuleBase<SocketCommandContext>
+        {
+            [Command("ChatGeral")]
+            public async Task DefinirChatGeralTask()
+            {
+                var guild = GuildsMannanger.GetGuild(Context.Guild);
+
+                guild.IdChatGeral = Context.Channel.Id;
+                GuildsMannanger.SaveGuilds();
+
+                await ReplyAsync("",false,EmbedHandler.CriarEmbed
+                    ("Definido",$"Canal de texto: ``{Context.Channel.Name}``\nId: ``{Context.Channel.Id}``\nFoi definido como **chat geral**", EmbedMessageType.Success));
+            }
+        }
+    }
+}
