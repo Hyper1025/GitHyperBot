c7d9697522ee3943fdb3d7d8ca55980a6e67c470
Hyper <leo_xarroweb@hotmail.com>
2018-09-02 23:48:52 -0300

    :fire: Muitas coisas removidas

--- GitHyperBot/Constantes.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GitHyperBot
-{
-    public static class Constantes
-    {
-        public static readonly ulong DailyGold = 600;
-        public static readonly uint ChanceDeGanharAposta = 55;
-    }
-}
